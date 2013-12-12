6229ae021b140027de33b51d65922cd63fa037b2
Nicolas Brassard <nico@nitriques.com>
2013-12-12 10:02:16 -0500

    Fix variable name

--- src/app.js
+++ src/app.js
@@ -130,13 +130,13 @@
 				//$(leavingPage.key()).hide();
 				
 				//set leaving page to be previous one
-				previous = leavingPage;
+				previousPage = leavingPage;
 				previousUrl = document.location.href.substring(document.location.protocol.length + 2 + document.location.host.length);
 				//clear leavingPage
 				leavingPage = null;
 				
 				//notify all module
-				App.modules.notify('page.leave', {page: previous});
+				App.modules.notify('page.leave', {page: previousPage});
 			},
 			_enterNext = function() {
 				// set the new Page as the current one
