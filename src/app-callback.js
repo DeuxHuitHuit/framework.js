6ab61f1e9cba42c81ccaeb3f71cd886688876e86
Nicolas Brassard <nico@nitriques.com>
2014-04-01 14:45:41 -0400

    Added the loaded utility

--- src/app-callback.js
+++ src/app-callback.js
@@ -19,6 +19,9 @@
 			if ($.isFunction(fx)) {
 				// IE8 does not allow null/undefined args
 				return fx.apply(this, args || []);
+				
+			} else if ($.isPlainObject(fx)) {
+				return fx;
 			}
 		} catch (err) {
 			var stack = err.stack;
@@ -29,12 +32,31 @@
 		return undefined;
 	};
 	
+	// external lib load check
+	var loaded = function (v, fx, delay, maxRetriesCount, counter) {
+		delay = Math.max(delay || 0, 100);
+		maxRetriesCount = maxRetriesCount || 10;
+		counter = counter || 1;
+		// get the value
+		var value = callback(v, [counter]);
+		// if the value exists
+		if (!!value) {
+			// call the function, with the value
+			return callback(fx, [value, counter]);
+		} else if (counter < maxRetriesCount) {
+			// recurse
+			setTimeout(loaded, delay, v, fx, delay, maxRetriesCount, counter + 1);
+		}
+	};
+	
 	/** Public Interfaces **/
 	global.App = $.extend(global.App, {
 		
 		// callback utility
-		callback: callback
+		callback: callback,
 		
+		// loaded utility
+		loaded: loaded
 	});
 	
 })(jQuery, window);
