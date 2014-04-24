2c8aad4fc2c14e37dfc485ab94f7d60db2ebe47b
Nicolas Brassard <nico@nitriques.com>
2014-04-24 15:02:35 -0400

    new build

--- dist/framework.js
+++ dist/framework.js
@@ -1,4 +1,4 @@
-/*! framework.js - v1.3.1 - build 84 - 2014-04-21
+/*! framework.js - v1.3.1 - build 95 - 2014-04-24
 * https://github.com/DeuxHuitHuit/framework.js
 * Copyright (c) 2014 Deux Huit Huit; Licensed MIT */
 /**
@@ -1101,9 +1101,16 @@
 				return !!(userAgent.match(/Android/i));
 			},
 			
+			isAndroidPhone: function (userAgent) {
+				userAgent = getUserAgent(userAgent);
+				return global.BrowserDetector.isAndroid(userAgent) &&
+					!!(userAgent.match(/mobile/i));
+			},
+			
 			isPhone : function (userAgent) {
 				userAgent = getUserAgent(userAgent);
 				return global.BrowserDetector.isOtherPhone(userAgent) || 
+					global.BrowserDetector.isAndroidPhone(userAgent) ||
 					global.BrowserDetector.isIphone(userAgent);
 			},
 			
@@ -1184,20 +1191,50 @@
 	// touch support
 	if ($.touchClick) {
 		var didMove = false;
+		var preventNextClick = false;
+		var lastTouch = {x: 0, y: 0};
+		var minMove = 10 * (window.devicePixelRatio || 1);
 		$(document).on('touchstart', function (e) {
 			didMove = false;
+			var touch = e.originalEvent.touches[0];
+			lastTouch.x = touch.screenX;
+			lastTouch.y = touch.screenY;
+			App.log('touchstart', lastTouch);
 		}).on('touchmove', function (e) {
+			var touch = e.originalEvent.changedTouches[0];
 			// only count move when one finger is used
-			didMove = e.originalEvent.changedTouches.length === 1;
+			didMove = e.originalEvent.changedTouches.length === 1 &&
+				// and if the gesture was more than accidental
+				(Math.abs(lastTouch.x - touch.screenX) > minMove ||
+				Math.abs(lastTouch.y - touch.screenY) > minMove);
+				
+			App.log('touchmove', lastTouch, didMove, touch.screenX, touch.screenY);
 		}).on('touchend', function (e) {
+			App.log('touchend', lastTouch, didMove);
+			var t = $(e.target);
+			var ignoreInputs = 'input, select, textarea';
+			var ignoreClass = '.ignore-mobile-click, .ignore-mobile-click *';
+			var mustBeIgnored = t.is(ignoreInputs) || t.is(ignoreClass);
+			
+			// prevent click only if not ignored
+			preventNextClick = $.ios && !mustBeIgnored;
+			
 			// do not count inputs
-			if (!didMove && !$(e.target).is('input, select, textarea')) {
+			if (!didMove && !mustBeIgnored) {
 				// prevent default right now
 				global.pd(e);
 				$(e.target).trigger($.click);
 				return false;
 			}
-			didMove = false;
+		}).on('click', function (e) {
+			App.log('real click');
+			var isNextClickPrevented = preventNextClick;
+			preventNextClick = false;
+			if (isNextClickPrevented) {
+				App.log('click prevented');
+				global.pd(e);
+			}
+			return !isNextClickPrevented;
 		});
 	}
 	
